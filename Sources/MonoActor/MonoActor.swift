5977b438a4c80ab4225bcbc97d5613e8c8b0e983
Theis Egeberg <theis.egeberg@gmail.com>
2023-08-29 13:46:20 +0200

    Added all files

--- Sources/MonoActor/MonoActor.swift
+++ Sources/MonoActor/MonoActor.swift
@@ -1,6 +1,42 @@
-public struct MonoActor {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import Foundation
+
+/// MonoActor will is a single item queue, it will cancel any ongoing tasks if another is attempted.
+/// This is useful for user interactions that needs to cancel any ongoing requests made by the user.
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
+public actor MonoActor {
+    
+    private var currentTaskCancellation:(() -> ())? = nil
+    
+    public init() {}
+    
+    /// Run a piece of code and cancel any ongoing pieces of code that is being run.
+    /// - Parameter f: The closure with the code to be run.
+    /// - Returns: The return value of the code being run.
+    public func run<T>(_ f: @escaping () async throws -> (T)) async throws -> T {
+        defer {
+            self.currentTaskCancellation = nil
+        }
+        if let currentTaskCancellation {
+            currentTaskCancellation()
+        }
+        let task = Task {
+            try await f()
+        }
+        currentTaskCancellation = task.cancel
+        return try await task.value
     }
+    
+    /// Run a piece of code and cancel any ongoing pieces of code that is being run.
+    /// - Parameter f: The closure with the code to be run.
+    public func run(_ f: @escaping () async throws -> ()) async throws {
+        try await self.run<Void>(f)
+    }
+    
+    public func cancel() {
+        currentTaskCancellation?()
+        currentTaskCancellation = nil
+    }
+    
 }
--- Tests/MonoActorTests/MonoActorTests.swift
+++ Tests/MonoActorTests/MonoActorTests.swift
@@ -2,10 +2,32 @@ import XCTest
 @testable import MonoActor
 
 final class MonoActorTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MonoActor().text, "Hello, World!")
+    func testMonoActor() async throws {
+        let queue = MonoActor()
+        let failureExpectations = expectation(description: "Expected failures")
+        failureExpectations.expectedFulfillmentCount = [1,3,5,7,9].count
+        let successExpectations = expectation(description: "Expected successes")
+        successExpectations.expectedFulfillmentCount = [0,2,4,6,8,10].count
+        
+        for x in 0...10 {
+            Task {
+                do {
+                    let result = try await queue.run {
+                        if x.isMultiple(of: 2) {
+                            return x
+                        } else {
+                            try await Task.sleep(nanoseconds: 1000)
+                            return x
+                        }
+                        
+                    }
+                    XCTAssertEqual(result, x)
+                    successExpectations.fulfill()
+                } catch {
+                    failureExpectations.fulfill()
+                }
+            }
+        }
+        await fulfillment(of: [successExpectations,failureExpectations], timeout: 4, enforceOrder: false)
     }
 }
